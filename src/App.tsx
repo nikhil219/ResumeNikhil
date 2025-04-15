0b1172d3cb4a085cd7c5792db0fe9f7f8b669428
Nikhil <nikhilmishra218@gmail.com>
2025-04-15 11:50:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,11 +42,11 @@ function App() {
       <nav className="fixed top-0 left-0 right-0 bg-white/70 backdrop-blur-sm z-50 border-b">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
-            <span className="text-xl font-bold">Nikhil Mishra</span>
+            {/* <span className="text-xl font-bold">Nikhil Mishra</span> */}
             <a
   href="#about"
-  className="inline-flex items-center gap-2 "
-/>Nikhil Mishra
+  className="inline-flex items-center gap-2 text-xl font-bold"
+>Nikhil Mishra</a>
             {/* Desktop Navigation */}
             <div className="hidden md:flex items-center space-x-8">
               <a
